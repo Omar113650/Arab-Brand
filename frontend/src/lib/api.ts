593bd8d3aaaa6c15255a58f0b7ed2a7698e56b01
Omar113650 <omarelhelaly520@gmail.com>
2026-06-28 02:11:19 +0300

    handle vercel

---
+++ frontend/src/lib/api.ts
@@ -0,0 +1,4 @@
+const BASE = "https://arab-brand-4qj7.vercel.app";
+
+export const apiFetch = (path: string, options?: RequestInit) =>
+  fetch(`${BASE}${path}`, { credentials: "include", ...options });
