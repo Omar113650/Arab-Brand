ad623f0f0d5665325fa170dd31cb07cb0c97d981
Omar113650 <omarelhelaly520@gmail.com>
2026-06-28 02:05:53 +0300

    handle vercel

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-console.log("🔥 VITE CONFIG LOADED");
-
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       "/api": {
-        target: "https://arab-brand-4qj7.vercel.app/",
+        target: "https://arab-brand-4qj7.vercel.app",
         changeOrigin: true,
       },
     },
